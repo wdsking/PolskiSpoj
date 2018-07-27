3835be18e164b96805edfa87f273362f18a31385
wdsking <19kubaj98@gmail.com>
2018-07-27 13:13:07 +0200

    Problem solved

---
+++ PP0501A - NWD/PP0501A - NWD/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PP0501A___NWD
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a=0, b = 0;
+			
+			if(a<0 || b<0 || a>1000000||b>1000000)
+			{
+				return;
+			}
+			int testsCount = int.Parse(Console.ReadLine());
+			for (int i = 0; i < testsCount; i++)
+			{
+				string[] text = Console.ReadLine().Split();
+				a = int.Parse(text[0]);
+				b = int.Parse(text[1]);
+				Console.WriteLine(nwd(a, b));
+			}
+		}
+		static int nwd(int a,int b)
+		{
+			if(b==0)
+			{
+				return a;
+			}
+			return nwd(b, a % b);
+		}
+		
+	}
+}
