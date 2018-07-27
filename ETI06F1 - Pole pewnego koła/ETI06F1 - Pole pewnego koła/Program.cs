78c6f54db1af3a73dd28f859a51dd6187d4d2520
wdsking <19kubaj98@gmail.com>
2018-07-27 19:05:08 +0200

    Solved

---
+++ ETI06F1 - Pole pewnego koła/ETI06F1 - Pole pewnego koła/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ETI06F1___Pole_pewnego_koła
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int r = 0, d = 0;
+			string[] text = Console.ReadLine().Split();
+			r = int.Parse(text[0]);
+			d = int.Parse(text[1]);
+			Console.WriteLine(Math.Round(((r*r) - ((d*d)/4))* 3.141592654, 2));
+		}
+	}
+}
