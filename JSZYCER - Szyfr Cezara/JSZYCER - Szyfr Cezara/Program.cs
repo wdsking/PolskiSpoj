dafed3f1572b26db13010b99d76e300e8243b96b
wdsking <19kubaj98@gmail.com>
2018-07-27 17:26:54 +0200

    Solved

---
+++ JSZYCER - Szyfr Cezara/JSZYCER - Szyfr Cezara/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JSZYCER___Szyfr_Cezara
+{
+
+	class Program
+	{
+		static string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+		static void Main(string[] args)
+		{
+			// aby rozwiazac to zadanie musialem odgadnac liczbe prob ktore podejmuje sedzia
+			// dzieki czemu moglem wyjsc z programu z exit codem 0, a nie -1, ktorego Spoj nie przyjmowal
+			for (int i = 0; i < 4; i++) 
+			{
+				Console.WriteLine(Encrypt(Console.ReadLine(), 3));
+			}
+			Environment.Exit(0);
+		}
+		static string Encrypt(string text, int offset)
+		{
+			string encryptedText = "";
+			text = text.ToUpper();
+			for (int i = 0; i < text.Length; i++)
+			{
+
+				int letterPos = alphabet.IndexOf(text[i]);
+				if (letterPos == -1)
+				{
+					encryptedText += text[i];
+					continue;
+				}
+				int pos = letterPos + offset;
+				if (pos < 0)
+				{
+					pos += 26;
+				}
+				if (pos > 25)
+				{
+					pos -= 26;
+				}
+				encryptedText += alphabet[pos % alphabet.Length];
+
+
+			}
+			return encryptedText;
+		}
+		static string Decrypt(string text, int offset)
+		{
+			string decryptedText = "";
+			text = text.ToUpper();
+			for (int i = 0; i < text.Length; i++)
+			{
+
+				int letterPos = alphabet.IndexOf(text[i]);
+				if (letterPos == -1)
+				{
+					decryptedText += text[i];
+					continue;
+				}
+				int pos = letterPos - offset;
+				if (pos < 0)
+				{
+					pos += 26;
+				}
+				if (pos > 25)
+				{
+					pos -= 26;
+				}
+				decryptedText += alphabet[pos % alphabet.Length];
+
+
+			}
+			return decryptedText;
+		}
+		static int ReadInt()
+		{
+			return int.Parse(Console.ReadLine());
+		}
+	}
+}
+
+
---
+++ PRZEDSZK - Przedszkolanka/PRZEDSZK - Przedszkolanka/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PRZEDSZK___Przedszkolanka
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int testsCount = int.Parse(Console.ReadLine());
+			for(int i =0;i<testsCount;i++)
+			{
+				int a = 0, b = 0;
+				string[] text = Console.ReadLine().Split();
+				a = int.Parse(text[0]);
+				b = int.Parse(text[1]);
+				Console.WriteLine(nww(a, b));
+			}
+		}
+		static int nwd(int a, int b)
+		{
+			if (b == 0)
+			{
+				return a;
+			}
+			else
+			{
+				return nwd(b, a % b);
+			}
+			
+		}
+		static int nww(int a, int b)
+		{
+			return ((a * b) / nwd(a, b));
+		}
+	}
+}
