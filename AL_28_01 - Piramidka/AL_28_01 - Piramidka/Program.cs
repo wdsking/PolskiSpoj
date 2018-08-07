dc29acf28c7de19edeb25466c069516c7c86b692
wdsking <19kubaj98@gmail.com>
2018-08-07 16:22:55 +0200

    Solved

---
+++ AL_28_01 - Piramidka/AL_28_01 - Piramidka/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AL_28_01___Piramidka
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				int wordLength = 0;
+				string word;
+				wordLength = int.Parse(Console.ReadLine());
+				word = Console.ReadLine();
+				for (int i = 0; i < wordLength; i += 2)
+				{
+					string result = "";
+					int letters = i + 1;
+					int dots = (wordLength - letters) / 2;
+					for (int a = 0; a < dots; a++)
+					{
+						result += ".";
+					}
+					for (int a = 0; a < letters; a++)
+					{
+						result += word[dots + a];
+					}
+					for (int a = 0; a < dots; a++)
+					{
+						result += ".";
+					}
+					Console.WriteLine(result);
+				}
+			}catch(Exception ex) { Environment.Exit(0); }
+
+		}
+	}
+}
---
+++ FR_09_06 - Odległość w wyrazach/FR_09_06 - Odległość w wyrazach/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FR_09_06___Odległość_w_wyrazach
+{
+	class Program
+	{
+		static string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		static void Main(string[] args)
+		{
+			int smallest=-1;
+			int largest=-1;
+			try
+			{
+				int tests;
+				tests = int.Parse(Console.ReadLine());
+				for (int i = 0; i < tests; i++)
+				{
+					string text = Console.ReadLine();
+					for (int a = 0; a < text.Length; a++)
+					{
+						if(a==0)
+						{
+							smallest = Alphabet.IndexOf(text[a]);
+							largest = Alphabet.IndexOf(text[a]);
+							continue;
+						}
+						if (Alphabet.IndexOf(text[a])<smallest)
+						{
+							smallest = Alphabet.IndexOf(text[a]);
+						}
+						if (Alphabet.IndexOf(text[a]) > largest)
+						{
+							largest = Alphabet.IndexOf(text[a]);
+						}
+					}
+					Console.WriteLine(largest - smallest);
+				}
+			}catch(Exception ex) { Environment.Exit(0); }
+		}
+	}
+}
