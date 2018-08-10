8a245f4b48a96784de343102b6ed5efeae8331d5
wdsking <19kubaj98@gmail.com>
2018-08-11 01:18:43 +0200

    Solved

---
+++ AL_10_01 - Sekretny kod CSHARP/AL_10_01 - Sekretny kod CSHARP/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace AL_10_01___Sekretny_kod_CSHARP
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				int tests;
+				tests = int.Parse(Console.ReadLine());
+				for (int i = 0; i < tests; i++)
+				{
+					string[] text = Console.ReadLine().Split();
+					int a = int.Parse(text[0]);
+					string b = text[1];
+					BigInteger counter = 1;
+
+						for (int x = 0; x < b.Length; x++)
+						{
+							if (b.Length == 1 && b[0]=='?')
+							{
+								counter *= 10;
+								break;
+							}
+							if (b[x] == '?')
+							{
+								if (x == 0)
+								{
+									counter *= 9;
+								}
+								else counter *= 10;
+							}
+						}
+					if (counter == 1) counter = 0;
+					
+					Console.WriteLine(counter);
+
+				}
+			}
+			catch (Exception ex) { Environment.Exit(0); }
+		}
+	}
+}
