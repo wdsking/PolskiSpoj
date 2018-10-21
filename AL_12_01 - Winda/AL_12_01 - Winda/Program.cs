1ac1aa7baeb79b1cc5133bda55e256b048208fb5
wdsking <19kubaj98@gmail.com>
2018-10-22 00:29:15 +0200

    Solved

---
+++ AL_12_01 - Winda/AL_12_01 - Winda/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace AL_12_01___Winda
+{
+    class Program
+    {
+        static string ValidateElevatorMovement(int floors, string movements)
+        {
+            int startFloor;
+            string result="NIE";
+            for (int f = 0; f < floors; f++)
+            {
+                startFloor = f+1;
+                for (int i = 0; i < movements.Length; i++)
+                {
+                    if(movements[i] == 'U')
+                    {
+                        startFloor++;
+                    }
+                    if(movements[i] == 'D')
+                    {
+                        startFloor--;
+                    }
+                    if(startFloor<1 || startFloor>floors)
+                    {
+                        result = "NIE";
+                        break;
+                    }else
+                    {
+                        result = "TAK";
+                    }
+                }
+                if(result == "TAK")
+                {
+                    return result;
+                }
+               
+            }
+            return result;
+          
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                int tests = int.Parse(Console.ReadLine());
+                for (int i = 0; i < tests; i++)
+                {
+                    int floors = int.Parse(Console.ReadLine());
+                    string movement = Console.ReadLine();
+                    Console.WriteLine(ValidateElevatorMovement(floors, movement));
+
+                }
+            }catch(Exception ex)
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ AL_17_01 - Mundial 2014/AL_17_01 - Mundial 2014/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AL_17_01___Mundial_2014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Mundial " + (int)ConsoleColor.DarkGreen + (int)ConsoleColor.Black + (int)ConsoleColor.DarkBlue + (int)ConsoleColor.DarkRed;
+            Console.WriteLine(text);
+        }
+    }
+}
