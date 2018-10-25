dc39c5b7124ac28383b0e32c6cae33c01786fd69
wdsking <19kubaj98@gmail.com>
2018-10-25 02:34:18 +0200

    Solved

--- PP0602B - Tabelki liczb/PP0602B - Tabelki liczb/Program.cs
+++ PP0602B - Tabelki liczb/PP0602B - Tabelki liczb/Program.cs
@@ -4,6 +4,68 @@ namespace PP0602B___Tabelki_liczb
 {
     class Program
     {
+        static void Rotate(int[,] table)
+        {
+            int x = 0, y = 1;
+            int previousNumber = table[0, 0];
+            while (true)
+            {
+                if (x == 0 && y == 0)
+                {
+                    int tmp = table[y, x];
+                    table[y, x] = previousNumber;
+                    previousNumber = tmp;
+                    x--;
+                    break;
+                }
+                if (y != table.GetLength(0) - 1 && x==0)
+                {
+                    int tmp = table[y,x];
+                    table[y,x] = previousNumber;
+                    previousNumber = tmp;
+                    y++;
+                    continue;
+                }
+                if(y==table.GetLength(0)-1 && x != table.GetLength(1) - 1)
+                {
+                    int tmp = table[y,x];
+                    table[y,x] = previousNumber;
+                    previousNumber = tmp;
+                    x++;
+                    continue;
+                }
+                if(x == table.GetLength(1) - 1 && y!=0)
+                {
+                    int tmp = table[y,x];
+                    table[y,x] = previousNumber;
+                    previousNumber = tmp;
+                    y--;
+                    continue;
+                }
+                if(y==0)
+                {
+                    int tmp = table[y,x];
+                    table[y,x] = previousNumber;
+                    previousNumber = tmp;
+                    x--;
+                    continue;
+                }
+               
+            }
+        }
+        static void Display(int[,]table)
+        {
+            for (int x = 0; x < table.GetLength(0); x++)
+            {
+             
+
+                for (int y = 0; y < table.GetLength(1); y++)
+                {
+                    Console.Write(table[x, y] + " ");
+                }
+                Console.Write("\n");
+            }
+        }
         static void Main(string[] args)
         {
             int t = int.Parse(Console.ReadLine());
@@ -24,7 +86,10 @@ namespace PP0602B___Tabelki_liczb
                         table[x,y] = int.Parse(text[y]);
                     }
                 }
+                Rotate(table);
+                Display(table);
             }
+
         }
     }
 }
