681ed3e8b05752ae9d4c93aac513324d426a7bed
Qb <19kubaj98@gmail.com>
2018-10-27 23:05:10 +0200

    Solved

---
+++ AL_30_01 - Anagramy/AL_30_01 - Anagramy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+namespace AL_30_01___Anagramy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            string word = input[0];
+            string sortedWord = String.Concat(word.OrderBy(c => c));
+            int possibleAnagrams = int.Parse(input[1]);
+            int counter = 0;
+            for (int i = 0; i < possibleAnagrams; i++)
+            {
+                string anagram = Console.ReadLine();
+                anagram = string.Concat(anagram.OrderBy(c => c));
+                if(sortedWord == anagram)
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
