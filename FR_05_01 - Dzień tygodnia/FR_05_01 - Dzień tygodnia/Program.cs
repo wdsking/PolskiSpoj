6b97a8f0824f439dd0f497e4633524ade53e6083
wdsking <19kubaj98@gmail.com>
2018-10-28 23:34:12 +0100

    Solved

---
+++ FR_05_01 - Dzień tygodnia/FR_05_01 - Dzień tygodnia/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace FR_05_01___Dzień_tygodnia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine());
+            for (int i = 0; i < tests; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string dayName = input[0];
+                int day=0;
+                int days = int.Parse(input[1]);
+                if(dayName == "Pn")
+                {
+                    day = 0;
+                }
+                if(dayName == "Wt")
+                {
+                    day = 1;
+                }
+                if(dayName == "Sr")
+                {
+                    day = 2;
+                }
+                if(dayName == "Cz")
+                {
+                    day = 3;
+                }
+                if(dayName == "Pt")
+                {
+                    day = 4;
+                }
+                if(dayName == "So")
+                {
+                    day = 5;
+                }
+                if(dayName == "Nd")
+                {
+                    day = 6;
+                }
+                day += days;
+                day %= 7;
+                if(day == 0)
+                {
+                    Console.WriteLine("Pn");
+                }if (day == 1)
+                {
+                    Console.WriteLine("Wt");
+                }
+                if(day == 2)
+                {
+                    Console.WriteLine("Sr");
+
+                }
+                if(day == 3)
+                {
+                    Console.WriteLine("Cz");
+
+                }
+                if(day == 4)
+                {
+                    Console.WriteLine("Pt");
+                }
+                if(day == 5)
+                {
+                    Console.WriteLine("So");
+                }
+                if(day == 6)
+                {
+                    Console.WriteLine("Nd");
+                }
+            }
+        }
+    }
+}
---
+++ SMWP_106 - Diament Bez Skazy/SMWP_106 - Diament Bez Skazy/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SMWP_106___Diament_Bez_Skazy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine());
+            for (int i = 0; i < tests; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                int n = 1;
+                while(true)
+                {
+                    int edges = (n*(n - 1)) / 2;
+                    if(edges== num)
+                    {
+                        Console.WriteLine("TAK");
+                        break;
+                    }
+                    if(edges>num)
+                    {
+                        Console.WriteLine("NIE");
+                        break;
+                    }
+                    n++;
+                }
+            }
+        }
+    }
+}
